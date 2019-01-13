de477cb9553cb4f7abca32d66e14f039df8b4150
DominikLipinski <maboonga@gmail.com>
2019-01-13 12:23:38 +0100

    first API call to food2fork.com (+corrected babel config)

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,9 @@
-// Global app controller
+import axios from 'axios';
+
+const getResults = async(query) => {
+    const key = '8fcb4a11834b907ae4e2d48ee3fc2fd6';
+    const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${query}`);
+    console.log(res);
+};
+
+getResults('pizza');
