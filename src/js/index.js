6584b8514b8cff66ef3f9c8b37e1ddcc68e1230f
DominikLipinski <maboonga@gmail.com>
2019-01-14 18:23:21 +0100

    search controller

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,33 @@
 import Search from './models/Search';
 
-const search = new Search('garlic');
-console.log(search);
-search.getResults();
+/* Global state:
+-search
+- current recipe
+- shopping list
+-liked recipes
+*/
+
+const state = {};
+
+const controlSearch = async () => {
+    //get query from view - TODO
+    const query = 'chicken';
+
+    //generate new search object and add it to state
+    if(query) {
+        state.search = new Search(query);
+
+    // prepare UI for results (cleaning)
+    
+    // perform search 
+       await state.search.getResults();
+
+    //display result
+        console.log(state.search.result);
+    }
+}
+
+document.querySelector('.search').addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+});
