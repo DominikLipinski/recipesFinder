b86080735662a3fa9e31177ff4d139bd799d2e8a
DominikLipinski <maboonga@gmail.com>
2019-01-22 07:14:07 +0100

    likes controller

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 import Search from './models/Search';
 import Recipe from './models/Recipe';
 import List from './models/List';
+import Likes from './models/Likes';
 import * as searchView from './views/searchView';
 import * as recipeView from './views/recipeView';
 import * as listView from './views/listView';
@@ -131,18 +132,30 @@ elements.shopping.addEventListener('click', e => {
 
 });
 
+/*-- LIKES CONTROLLER --*/
+
+const controlLike = () => {
+
+};
+
 // recipe servings button clicks
 elements.recipe.addEventListener('click', e => {
     if(e.target.matches('.btn-decrease, .btn-decrease *')) {
+        //decrease button
         if(state.recipe.servings > 1) {
             state.recipe.updateServings('dec');
             recipeView.updateServingsIngredients(state.recipe);
         }
     } else if(e.target.matches('.btn-increase, .btn-increase *')) {
+        //increase button
         state.recipe.updateServings('inc');
         recipeView.updateServingsIngredients(state.recipe);
     } else if(e.target.matches('.recipe__btn--add, .recipe__btn--add *')) {
+        // add ingredients to shopping list
         controlList();
+    } else if (e.target.matches('.recipe__love, .recipe__love *')) {
+        // like controller
+        controlLike();
     }
 });
 
